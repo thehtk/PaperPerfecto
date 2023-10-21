398168b083bfbf9f78b715cbc82d4d2b3aa877cf
HITESH KUMAR <73693548+thehtk@users.noreply.github.com>
2023-10-22 01:26:13 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -5,6 +5,7 @@ from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Inches
 import helper
 import re
+import warnings
 
 st.set_page_config(
     page_title="Document Creator",
